ae83b618e3cbf448c6973ab36bc3e7f67395153c
Jung-Min Ha <67835149+hjmthebest@users.noreply.github.com>
2023-04-24 17:57:42 +0900

    Update Department_Store_VIP_Enuri_Report.py

--- Department_Store_VIP_Enuri_Report.py
+++ Department_Store_VIP_Enuri_Report.py
@@ -1,26 +1,17 @@
 ```python
 
-# -- #########################################################################################
-# -- ################                                                         ################
-# -- ################                 에누리 소진율 현황 리포트                 ################
-# -- ################                                                         ################
-# -- #########################################################################################
+# #########################################################################################
+# ################                                                         ################
+# ################                  에누리소진율 현황 리포트                 ################
+# ################                                                         ################
+# #########################################################################################
 
 ##############################################
-###### 테스트용 리포트 필터 조건 임의 설정 ######
+##### 테스트를 위한 리포트 필터 임의 설정 #####
 ##############################################
-
-# start_date = input("Please insert start date (yyyy-MM-dd): ")
-# end_date = input("Please insert end date (yyyy-MM-dd): ")
-# selstr_cd = input("Please insert store code (4 digits): ")
-# sign = input("Sign: ")
-# percentage = int(input("Percentage of enuri"))
-
-start_date = '2023-01-01'
-end_date = '2023-04-20'
-selstr_cd = '0001'
-sign = '>'
-percentage = '90'
+start_date = '2022-01-01'
+end_date = '2022-12-31'
+selstr_cd = '0002'
 ##############################################
 
 enuri = spark.sql(f"""
@@ -29,9 +20,9 @@ enuri = spark.sql(f"""
              , a.selstr_cd                                              -- 선정점코드
              , a.cust_grde_cd                                           -- 고객등급
              , a.max_rdcr                                               -- 최대에누리율
-             , ifnull(a.lmt_bsc_amt + a.lmt_add_amt, 0) as lmt_ttl_amt  -- 지급에누리총액(한도+추가)
+             , ifnull(a.lmt_bsc_amt + a.lmt_add_amt, 0) as lmt_ttl_amt  -- 지급에누리총액(한도 + 추가)
              , ifnull(a.lmt_bsc_amt, 0) as lmt_bsc_amt                  -- 한도기본금액(한도에누리)
-             , ifnull(a.lmt_add_amt, 0) as lmt_add_amt                  -- 한도추가금액(추가에누리: 스타포인트를 전환하여 추가 or Upsell 프로모션 위한 추가 지급)
+             , ifnull(a.lmt_add_amt, 0) as lmt_add_amt                  -- 한도추가금액(추가에누리: 스타포인트를 한도에누리 전환하여 기존 한도에 추가 or Upsell 프로모션 위한 추가 지급)
         from TB_DWCU_NWRDCTLMTCUST_M a                                  -- DWCU_신에누리한도고객_마스터
         where 1 = 1
             and a.lmt_slct_yy = '{start_date[:4]}'
@@ -52,7 +43,7 @@ enuri = spark.sql(f"""
         from TB_DWSL_NWRDCTCPNUSE_L b                                   -- DWSL_신에누리쿠폰사용_내역
         where 1 = 1
             and b.trns_dt between '{start_date[:4]}-01-01' and '{start_date[:4]}-12-31'         -- 거래일자 범위: 설정년도 
-            and b.cpn_evt_no like '9%'                                  -- 우수고객 한도/상시
+            and b.cpn_evt_no like '9%'                                                          -- 우수고객 한도/상시
             and b.rdct_lmt_amt != 0
 
     ), enuri_cncl_log as (
@@ -65,12 +56,12 @@ enuri = spark.sql(f"""
              , ifnull(-c.rdct_lmt_amt, 0) as rdct_lmt_amt_day           -- 에누리한도금액 중 취소금액(일자별)
              , ifnull(c.gs_slng_amt, 0) as gs_slng_amt                  -- 총매출금액
              , ifnull(c.rdct_obj_amt, 0) as rdct_obj_amt                -- 에누리대상금액
-             , ifnull(c.rdct_amt, 0) as rdct_amt                        -- 에누리금액
+             , ifnull(c.rdct_amt, 0) as rdct_amt                        -- 에누리금액gks
              , ifnull(c.pslng_amt, 0) as pslng_amt                      -- 순매출금액
         from TB_DWSL_NWRDCTCPNUSECANC_L c                               -- DWSL_신에누리쿠폰사용취소_내역
         where 1 = 1
             and c.trns_dt between '{start_date[:4]}-01-01' and '{start_date[:4]}-12-31'         -- 거래취소일자 범위: 설정년도 
-            and c.cpn_evt_no like '9%'                                  -- 우수고객 한도/상시
+            and c.cpn_evt_no like '9%'                                                          -- 우수고객 한도/상시
             and c.rdct_lmt_amt != 0
 
     ), tb_base as (
@@ -87,50 +78,54 @@ enuri = spark.sql(f"""
                     when t1.cust_grde_cd = '51' then 'AVE-GREEN2'
                     when t1.cust_grde_cd = '52' then 'AVE-GREEN3'
                     when t1.cust_grde_cd = '53' then 'PRE-GREEN'
-                    else 'Customer' end as cust_grde                    -- 고객등급 (조회시점 등급 기준, 연내 등급 변동되어도 현재등급기준)
+                    else 'Customer' end as cust_grde                    -- 고객등급 (조회시점 등급 기준, 연내 등급 바뀌어도 현재 등급 기준)
              , t2.trns_dt                                               -- 거래일자
              , lpad(cast(t2.cstr_cd as string), "4", "0") as cstr_cd    -- 자점코드             
              , t1.max_rdcr                                              -- 최대에누리율
-             , t1.lmt_ttl_amt                                           -- 지급에누리총액(한도+추가)
+             , t1.lmt_ttl_amt                                           -- 지급에누리총액(한도 + 추가)
              , t1.lmt_bsc_amt                                           -- 한도기본금액(한도에누리)
-             , t1.lmt_add_amt                                           -- 한도추가금액(추가에누리: 스타포인트를 전환하여 추가 or Upsell 프로모션 위한 추가 지급)
+             , t1.lmt_add_amt                                           -- 한도추가금액(추가에누리: 스타포인트를 한도에누리로 전환하여 기존 한도에 추가 or Upsell 프로모션으로 위한 추가 지급)
 
              , lpad(cast(t2.pos_no as string), "4", "0") as pos_no      -- POS번호
              , lpad(cast(t2.rcpt_no as string), "4", "0") as rcpt_no    -- 영수증번호
              , t2.cpn_evt_no                                            -- 쿠폰이벤트번호
 
-             , t2.rdct_lmt_amt_day                                      -- 에누리한도금액(일자별)
-             , t2.gs_slng_amt                                           -- 총매출금액(일자별)
-             , t2.rdct_obj_amt                                          -- 에누리대상금액(일자별)
-             , t2.rdct_amt                                              -- 에누리금액(일자별)
-             , t2.pslng_amt                                             -- 순매출금액(일자별)
-
+             , t2.rdct_lmt_amt_day                                     -- 에누리한도금액(일자별)
+             , t2.gs_slng_amt                                          -- 총매출금액(일자별)
+             , t2.rdct_obj_amt                                         -- 에누리대상금액(일자별)
+             , t2.rdct_amt                                             -- 에누리금액(일자별)
+             , t2.pslng_amt                                            -- 순매출금액(일자별)
         from enuri_standard t1
         left join ((select b.* from enuri_used_log b) union (select c.* from enuri_cncl_log c)) t2
             on t1.dpstr_cust_no = t2.dpstr_cust_no
-        where 1 = 1
-    )
-    select
-    from (
+
+    ), tb_base_v2 as (
         select t.dpstr_cust_no
              , t.selstr_cd
+             , case when t.selstr_cd = t.cstr_cd then 1 else 0 end as selstr_yn
              , t.cust_grde
              , t.max_rdcr
              , t.lmt_ttl_amt
              , t.lmt_bsc_amt
              , t.lmt_add_amt
-             , sum(t.rdct_lmt_amt_day) as enuri_used_amt
-             , round((sum(t.rdct_lmt_amt_day) / t.lmt_ttl_amt) * 100, 1) as used_enuri_rt    -- 에누리소진율(%)
-             , sum(t.gs_slng_amt) as pch_amt
-             , sum(t.pslng_amt) as pslng_amt
+             
+             , sum(sum(t.rdct_lmt_amt_day)) over(partition by t.dpstr_cust_no) as enuri_usage_amt                                     -- 에누리사용금액
+             , round(((sum(sum(t.rdct_lmt_amt_day)) over(partition by t.dpstr_cust_no)) / t.lmt_ttl_amt) * 100, 1) as enuri_usage_rt  -- 에누리소진율(%)
+             
+             , sum(t.rdct_lmt_amt_day) as str_enuri_usage_amt                                                                         -- 관리점에누리사용금액
+             , round((sum(t.rdct_lmt_amt_day) / t.lmt_ttl_amt) * 100, 1) as str_enuri_usage_rt                                        -- 관리점에누리소진율(%)      
         from tb_base t
         where 1 = 1
-            and t.trns_dt between '{start_date}' and '{end_date}'           -- 에누리 거래일자 범위 지정: (필수 지정값)
-            and t.selstr_cd = '{selstr_cd}'                                 -- 리포트 필터 내 여러 점포도 지정할 수 있도록 조치 필요 (DEFAULT: 전점)
-        group by 1, 2, 3, 4, 5, 6, 7
-        having used_enuri_rt {sign} {percentage}                            -- 리포트 필터 내 메트릭 범위 지정할 수 있도록 조치 필요 (DEFAULT: 전체)
+            and t.trns_dt between '{start_date}' and '{end_date}'            -- 에누리 거래일자 범위 지정: (필수 지정값)
+            and t.selstr_cd = '{selstr_cd}'                                  -- 리포트 필터 내 여러 점포도 지정할 수 있도록 조치 필요 (DEFAULT: 전점)
+            and t.selstr_cd != '0000'                                        -- 본사 제외
+        group by 1, 2, 3, 4, 5, 6, 7, 8
         order by 1
-        ) s
+    )
+    select s.*
+    from tb_base_v2 s
+    where 1 = 1
+        and s.selstr_yn = 1
     """)
 
 enuri.createOrReplaceTempView("enuri")
